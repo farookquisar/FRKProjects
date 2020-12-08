b6512b57a32a2f2fe394a144fec1df234f991a9e
farookquisar <59113563+farookquisar@users.noreply.github.com>
2020-12-08 06:16:36 +0300

    1

---
+++ 4__AmazoneClone/amazone-clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDH06NqslKOhD6z11jpNk_CwcUN9MthUbY",
+  authDomain: "e-challenge-77bd9.firebaseapp.com",
+  projectId: "e-challenge-77bd9",
+  storageBucket: "e-challenge-77bd9.appspot.com",
+  messagingSenderId: "565069835053",
+  appId: "1:565069835053:web:8c33e437581f31427801a4",
+  measurementId: "G-F3M72FB9J1"
+};
